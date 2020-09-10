1b31288e0301e6dd53bfd86b182e666f7a85ad03
roni-kemp <roni.kemp@gmail.com>
2020-09-10 16:27:49 +0300

    Update get_files.py

--- get_files.py
+++ get_files.py
@@ -15,19 +15,41 @@ exp_folder_lst = str(os.listdir(prop_exp_folder))
 regex = r"[0-9]{6}_contin[_]*[low]*"
 matches = re.finditer(regex, exp_folder_lst, re.MULTILINE)
 
-
+lst = []
 for match in matches:
     per_exp_path = prop_exp_folder+"\\"+match.group()
-
-    for dirname, dirnames, filenames in os.walk(full_path):
+    exp_date = match.group()
+    lst.append(exp_date)
+    for dirname, dirnames, _ in os.walk(per_exp_path):
         # print path to all subdirectories first.
         for subdirname in dirnames:
             if subdirname.upper()[:6] == "CROPED":
                 per_location_path = os.path.join(dirname, subdirname)
 
-                for dirname, dirnames, filenames in os.walk(per_location_path):
-                    print(filenames[0])
-                    print(filenames[-1])
+                O_L_regex = r"(_[L,R])\\Croped(_[1-5])"
+                O_L_match = re.search(O_L_regex, per_location_path)
+                if O_L_match:
+                    orient = O_L_match.group(1)
+                    loc = O_L_match.group(2)
+
+                    new_folder = exp_date + orient
+                    rel_path = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\first_last_imgs"
+                    new_folder =  os.path.join(rel_path, new_folder)
+                    if not os.path.isdir(new_folder):
+                        os.mkdir(new_folder)
+
+                    filenames = os.listdir(per_location_path)
+                    firs_img_path = os.path.join(per_location_path, filenames[0])
+                    last_img_path = os.path.join(per_location_path, filenames[-1])
+
+                    first_new_name = filenames[0].strip(".jpg") + loc + ".jpg"
+                    first_new_path = os.path.join(new_folder, first_new_name)
+                    last_new_name = filenames[-1].strip(".jpg") + loc + ".jpg"
+                    last_new_path = os.path.join(new_folder, last_new_name)
+
+
+                    os.system(r'copy "{in_path}" "{out_path}"'.format(in_path = firs_img_path, out_path = first_new_path))
+                    os.system(r'copy "{in_path}" "{out_path}"'.format(in_path = last_img_path, out_path = last_new_path))
 
 
 #%% usfull
