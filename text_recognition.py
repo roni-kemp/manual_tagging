2fcf011d9b321b25cf44eb6e30494d8f868b27ed
roni-kemp <roni.kemp@gmail.com>
2021-03-30 11:45:45 +0300

    combined result with tag file

--- text_recognition.py
+++ text_recognition.py
@@ -6,13 +6,12 @@ Created on Mon Mar 29 13:57:54 2021
 """
 import os
 import cv2
-import numpy as np
-from pathlib import Path
-import time
-#%%
+from ast import literal_eval
+import pandas as pd
+
+
 
 def detect_cotil_text(img, file_name = "test"):
-
     ## since "closed" is loger we jsut look at the end of the area it could be in
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray_ROI = gray_img[0:20,66:76]
@@ -34,56 +33,60 @@ def detect_cotil_text(img, file_name = "test"):
     elif binary_roi[11,34] == 255:
         return "Hook"
 
-#%% testing dir
-directory = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\result_imgs\200915_contin_low_L"
-lst = os.listdir(directory)
-for file_name in lst:
-    full = directory + "\\" + file_name
-    img = cv2.imread(full, cv2.IMREAD_COLOR)
-    state  = detect_cotil_text(img, file_name)
-    print(file_name+"  ---   "+state)
-    ## show the part of the img with the text
-#    ROI = img[0:22,18:76]
-#    cv2.namedWindow(file_name, cv2.WINDOW_NORMAL)
-#    cv2.imshow(file_name , ROI)
+#%%
+
+
+## load in the data from the .txt file
+with open(r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\TEST.txt", "r") as tags_file:
+    ## get the first line to init the dct for the data frame
+    f_line = tags_file.readline()
+    f_dct = literal_eval(f_line)
+    ## init dataframe with dct keys + broken down file name and dict name
+    ## (from the full_img_path)
+
+    df = pd.DataFrame(columns=f_dct.keys())
+    df["file_name"] = ""
+    df["directory_name"] = ""
+
+    df = df.append(f_dct, ignore_index=True)
+
+    for line in tags_file:
+        line = line.strip()
+        dct = literal_eval(line)
+        df = df.append(dct, ignore_index=True)
+
+for i in range(len(df)):
+    df["file_name"][i]=df["full_img_path"][i].split("\\")[-1]
+    df["directory_name"][i]=df["full_img_path"][i].split("\\")[-2]
 
     #%%
 
 
-def crop_text_roi(img, window_name="croped"):
-    ROI = img[0:22,18:62]
 
 
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-#    for i in range(3):
-#        for j in range(3):
-#            cv2.circle(ROI, (8+j,13+i), 1, (0, 5, 250), -1)
+mother_dir = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\result_imgs"
+
+dir_lst = os.listdir(mother_dir)
+for directory_name in dir_lst:
+    directory_path = mother_dir + "\\" + directory_name
+    file_lst = os.listdir(directory_path)
+    for file_name in file_lst:
+        full = directory_path + "\\" + file_name
+        #%
+        img = cv2.imread(full, cv2.IMREAD_COLOR)
+        state  = detect_cotil_text(img, file_name)
+        if state == None:
+            continue
+        print(file_name+"  ---   "+state)
+
+        cur_dir_df = df[df["directory_name"]==directory_name]
+        ind = cur_dir_df[cur_dir_df["file_name"]==file_name].index[0]
+
+        df.loc[ind]["cot_stage"]=state
 
 
-    cv2.circle(ROI, (34,11), 1, (0, 5, 250), -1)
-    cv2.imshow(window_name , ROI)
-    cv2.waitKey(1)
-    return ROI
 
-hook_path = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\result_imgs\200827_contin_low_R\7284_CROPED_1.jpg"
-open_path = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\result_imgs\200827_contin_low_R\7284_CROPED_5.jpg"
-closed_path = r"C:\Users\YasmineMnb\Desktop\Roni_new\python scripts\manual_tagging\taged results\result_imgs\200827_contin_low_R\7284_CROPED_3.jpg"
-
-h_img = cv2.imread(hook_path, cv2.IMREAD_COLOR)
-o_img = cv2.imread(open_path, cv2.IMREAD_COLOR)
-c_img = cv2.imread(closed_path, cv2.IMREAD_COLOR)
-
-#crop_text_roi(h_img, "H")
-#crop_text_roi(c_img, "C")
-#crop_text_roi(o_img, "O")
-
-#%%
-
-
-def sim():
-    pos = (20,20)
-    font = 1
-    font_size = 1
-    font_thickness = 1
-
-    cv2.putText(img, cot_stage, pos, font, font_size,(90,255,30),font_thickness)
+        ## show the part of the img with the text
+    #    ROI = img[0:22,18:76]
+    #    cv2.namedWindow(file_name, cv2.WINDOW_NORMAL)
+    #    cv2.imshow(file_name , ROI)
